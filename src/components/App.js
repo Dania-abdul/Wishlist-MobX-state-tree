d96f7df77a7be1d0405fcccdeb2c6ab0d5e24bba
Dania Abdul <dania@hoppinger.com>
2022-09-01 13:27:53 +0000

    Create 3 files

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    render() {
+        return ()
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to react</h1>
+                </header>
+                <p className="App-intro">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Vivamus vitae semper velit. Curabitur at lectus eu sapien commodo dignissim
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
